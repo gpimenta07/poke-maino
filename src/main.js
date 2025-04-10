cf0f9f1efbe573dc3e732ba2c35c7248f8aa7735
Gabriel Pimenta <gabrielpimentauitec@gmail.com>
2025-04-10 11:24:54 -0300

    Deploy

--- src/main.js
+++ src/main.js
@@ -4,17 +4,16 @@ import "./style.css";
 import App from "./App.vue";
 import router from "./router";
 import i18n from "./i18n/i18n";
-import AOS from 'aos'
-import 'aos/dist/aos.css'
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { createPinia } from "pinia";
 
-
-
-AOS.init()
+AOS.init();
 const app = createApp(App);
-
-
+const pinia = createPinia();
+app.use(pinia);
 
 app.use(router);
-app.use(i18n); // <-- esse tem que vir ANTES do mount
+app.use(i18n); 
 
 app.mount("#app");
